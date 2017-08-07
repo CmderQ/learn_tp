389e91ea6687ffe19a7c55b5e8f97476dac38741
Cmder <CmderQ@users.noreply.github.com>
2017-08-08 00:32:20 +0800

    登录控制器以及页面

---
+++ app/Home/Controller/LoginController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+/**
+ * 前台登录页面
+ */
+
+class LoginController extends Controller {
+
+    public function index(){
+        $this->display();
+    }
+}
