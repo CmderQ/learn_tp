fb44398bdca1f25a3336fcc5cd5cb1d372a35941
Cmder <CmderQ@users.noreply.github.com>
2017-08-12 00:03:52 +0800

    注册登录控制器

--- app/Home/Controller/LoginController.class.php
+++ app/Home/Controller/LoginController.class.php
@@ -6,9 +6,18 @@ use Think\Controller;
  * 登录页面
  */
 
-class LoginController extends Controller {
-	
-    public function index(){
+class LoginController extends Controller
+{
+
+    //注册登录首页
+    public function index()
+    {
         $this->display();
+    }
+
+    //注册方法
+    public function register()
+    {
+        $this->redirect('index');
     }
 }
