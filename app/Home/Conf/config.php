759c89d88c21f37afb6c6a3144c4016f39df1838
CmderQ <CmderQ@users.noreply.github.com>
2017-08-11 20:02:45 +0800

    js,css,images路径设置

--- app/Home/Conf/config.php
+++ app/Home/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
     //'配置项'=>'配置值'
         'TMPL_PARSE_STRING'  =>array(
-             '__CSS__' => __ROOT__.'/Public/css',
-             '__JS__'     =>__ROOT__.'/Public/js',
-             '__BOOTSTRAP__' =>__ROOT__.'/Public/bootstrap',
+             '__CSS__' => __ROOT__.'/Public/static/css',
+             '__JS__'     =>__ROOT__.'/Public/static/js',
+             '__IMAGES__'=>__ROOT__.'/Public/static/images',
         ),
 
 );
