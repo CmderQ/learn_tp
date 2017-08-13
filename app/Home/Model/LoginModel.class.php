8a73c8919ecb6e305f03e8f28bcfae320470efda
Cmder <CmderQ@users.noreply.github.com>
2017-08-13 23:49:34 +0800

    login表操作

--- app/Home/Model/LoginModel.class.php
+++ app/Home/Model/LoginModel.class.php
@@ -9,6 +9,7 @@ use Think\Model\RelationModel;
 
 class LoginModel extends RelationModel
 {
+    //注册方法
     public function register($username, $password, $email)
     {
         $data = array(
@@ -23,6 +24,19 @@ class LoginModel extends RelationModel
             if ($uid) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    //查找方法
+    public function selectInfo($where)
+    {
+        if(empty($where)){
+            return false;
+        }
+        $result = $this->select($where);
+        if($result){
+            return true;
         }
         return false;
     }
