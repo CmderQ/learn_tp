3670bd63d8a1bccdff5494b5d4c1f248ebb9dde0
Cmder <CmderQ@users.noreply.github.com>
2017-08-25 22:20:43 +0800

    前台首页

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -12,7 +12,8 @@ class IndexController extends Controller
 
     public function index()
     {
-
-        echo '这是前台首页';
+        if($_SESSION['user_name'] == ''){
+            $this->redirect("/login/index", '',1, '请先进行登录...');
+        }
     }
 }
