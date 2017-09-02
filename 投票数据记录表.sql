64135c46e3c95bac5baea634c003831392cc62db
Cmder <CmderQ@users.noreply.github.com>
2017-09-03 00:35:50 +0800

    数据表

---
+++ 投票数据记录表.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.5-10.1.21-MariaDB : Database - user
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`user` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `user`;
+
+/*Table structure for table `cms_vote` */
+
+DROP TABLE IF EXISTS `cms_vote`;
+
+CREATE TABLE `cms_vote` (
+  `IDX` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` bigint(11) unsigned NOT NULL COMMENT '对应图片id',
+  `votenumber` bigint(11) unsigned NOT NULL COMMENT '票数',
+  `url` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '图片存储地址',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`IDX`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `cms_vote` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
