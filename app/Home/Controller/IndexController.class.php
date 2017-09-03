31a8070fdd277bc5cb272f8d3a6e6a65983075ce
Cmder <CmderQ@users.noreply.github.com>
2017-09-03 23:47:53 +0800

    前台首页控制器

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * 投票操作控制器
+ */
 
 namespace Home\Controller;
 
@@ -10,11 +13,52 @@ use Think\Controller;
 class IndexController extends Controller
 {
 
+    /**
+     * @var /Home/Model/VoteModel
+     */
+    protected $voteservice;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->voteservice = D('Vote');
+    }
+
     public function index()
     {
-//        if($_SESSION['user_name'] == ''){
-//            $this->redirect("/login/index", '',1, '请先进行登录...');
-//        }
+        $number = I('post.number', 0, 'intval');
+
+        if (!empty($number)) {
+            $number = $this->checkInput($number);
+            $resultGetInfo = $this->voteservice->getInfo($number);
+
+            //根据图片id，判断数据库中是否存在对于id的数据，若没有，则说明是第一次投票，进行添加操作，反之，则进行更新操作
+            if (empty($resultGetInfo)) {
+                $resultAddInfo = $this->voteservice->addData($number);
+                if (!$resultAddInfo) {
+                    $this->error('初次投票失败，请重试!');
+                }
+            } else {
+                $resultUdpateInfo = $this->voteservice->updateVote($number, $resultGetInfo['votenumber']);
+                if (!$resultUdpateInfo) {
+                    $this->error('投票失败，请重试!');
+                }
+            }
+
+            $this->success('投票成功，谢谢合作！');
+        }
+
         $this->display();
+    }
+
+    //检查传入的参数
+    private function checkInput($num)
+    {
+        $betwwenArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        if (!is_numeric($num) || !in_array($num, $betwwenArr)) {
+            $this->error('您选择的图片有问题， 请重新选择!');
+        }
+
+        return $num;
     }
 }
