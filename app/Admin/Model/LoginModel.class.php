29c2b21157effbbf4615b1afca6c7a0d0fbdb96c
Cmder <CmderQ@users.noreply.github.com>
2017-09-23 00:33:37 +0800

    后台登录模型层

---
+++ app/Admin/Model/LoginModel.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 注册登录操作
+ */
+
+namespace Home\Model;
+
+use Think\Model;
+
+class LoginModel extends Model
+{
+    //注册方法
+    public function register($username, $password, $email)
+    {
+        $data = array(
+            'user_name' => $username,
+            'password' => $password,
+            'email' => $email,
+            'creatime' => date("Y-m-d H:i:s"),
+        );
+
+        if ($this->create($data)) {
+            $uid = $this->add();
+            if ($uid) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //查找方法
+    public function getInfo($where)
+    {
+        if(empty($where)){
+            return false;
+        }
+        $result = $this->field('id')->where($where)->find();
+        if($result){
+            return true;
+        }
+        return false;
+    }
+}
