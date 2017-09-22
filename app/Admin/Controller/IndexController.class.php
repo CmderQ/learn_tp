ee5eba6c1f267b5d7464cd7c87aa357377421ee2
Cmder <CmderQ@users.noreply.github.com>
2017-09-23 00:23:29 +0800

    后台控制器首页

--- app/Admin/Controller/IndexController.class.php
+++ app/Admin/Controller/IndexController.class.php
@@ -3,6 +3,7 @@
 namespace Admin\Controller;
 
 use Think\Controller;
+use Think\Verify;
 
 /**
  * 首页
@@ -21,5 +22,22 @@ class IndexController extends Controller
     public  function login()
     {
         $this->display();
+    }
+
+    /**
+     * 验证码生成
+     */
+    public function generatorVerfiy()
+    {
+        $config = array(
+            'fontSize' => 24,    // 验证码字体大小
+            'length' => 4,     // 验证码位数
+            'useNoise' => false, // 关闭验证码杂点
+        );
+
+        $verify = new Verify($config);
+        $verify->imageW = 158;//验证码宽度
+        $verify->imageH = 41;//验证码高度
+        $verify->entry();
     }
 }
