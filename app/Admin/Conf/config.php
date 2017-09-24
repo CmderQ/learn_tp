371e8081c609e7e396068dfb62571e8c23b727a2
Cmder <CmderQ@users.noreply.github.com>
2017-09-25 00:01:40 +0800

    后台系统配置

--- app/Admin/Conf/config.php
+++ app/Admin/Conf/config.php
@@ -10,4 +10,8 @@ return array(
     'USER_AUTH_KEY'=>'uid',             //存储在session中的识别号
     'NOT_AUTH_MODULE'=>'Index',              //无需验证的控制器
     'NOT_AUTH_ACTION'=>'add_role_handle',              //无需验证的方法
+    'RBAC_ROLE_TABLE'=>'cms_role',      //角色表名称
+    'RBAC_USER_TABLE'=>'cms_role_user', //角色与用户的中间表名称（注意）
+    'RBAC_ACCESS_TABLE'=>'cms_access',  //权限表名称
+    'RBAC_NODE_TABLE'=>'cms_node',      //节点表名称
 );
